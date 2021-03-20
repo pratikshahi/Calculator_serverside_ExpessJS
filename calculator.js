f7b26cc3d4ca58f1d821ec0132896f6f01009c31
Pratik Shahi <shahipr0@gmail.com>
2021-03-20 08:38:48 +0530

    sending file to get request

--- calculator.js
+++ calculator.js
@@ -7,5 +7,6 @@ app.listen(3000,function(){
     console.log("server started on port 3000");
 });
 app.get("/",function(req,res){
-    res.send("Hello World");
+    //__dirname gives filepath
+    res.sendFile(__dirname+"/index.html");
 });
