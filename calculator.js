c4de82177997e6e811c0d2716a0363c062608a50
Pratik Shahi <shahipr0@gmail.com>
2021-03-20 09:27:23 +0530

    BMI addition

--- calculator.js
+++ calculator.js
@@ -24,4 +24,18 @@ app.post("/",function(req,res){
 
 
     res.send("Result of the calculation is "+result);
-})
+});
+
+//BMI calculator
+
+app.get("/bmiCalculator",function(req,res){
+    res.sendFile(__dirname+"/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator",function(req,res){
+    var weight=Number(req.body.weight);
+    var height=Number(req.body.height);
+    var bmi=(weight/(height*height));
+
+    res.send("Your BMI is "+bmi);
+});
