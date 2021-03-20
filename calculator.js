8d676b5c6ea73f9e5cd4b5e5712e9259b56d7fe3
Pratik Shahi <shahipr0@gmail.com>
2021-03-20 08:19:58 +0530

    after express setup

---
+++ calculator.js
@@ -0,0 +1,11 @@
+//jshint esversion:6
+
+const express=require("express");
+const app=express();
+
+app.listen(3000,function(){
+    console.log("server started on port 3000");
+});
+app.get("/",function(req,res){
+    res.send("Hello World");
+});
