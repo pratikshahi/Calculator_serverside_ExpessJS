a8639708ebb83758215bd67e7204103618dd0100
Pratik Shahi <shahipr0@gmail.com>
2021-03-20 09:03:10 +0530

    body-parser use and post

--- calculator.js
+++ calculator.js
@@ -1,7 +1,11 @@
 //jshint esversion:6
 
 const express=require("express");
+const bodyParser=require("body-parser");
+
 const app=express();
+//merding bodyparser into app //urlencoded means geting data from html
+app.use(bodyParser.urlencoded({extended: true}));     
 
 app.listen(3000,function(){
     console.log("server started on port 3000");
@@ -10,3 +14,14 @@ app.get("/",function(req,res){
     //__dirname gives filepath
     res.sendFile(__dirname+"/index.html");
 });
+
+//response to post action
+app.post("/",function(req,res){
+    //req.body.num1 gets value from index.html.num1 input in char format
+    var num1=Number(req.body.num1);
+    var num2=Number(req.body.num2);
+    var result=num1+num2;
+
+
+    res.send("Result of the calculation is "+result);
+})
